a8d30333aec5026d566d53a28c484929285ff291
Kacper Cybula <kscybula@gmail.com>
2025-01-25 22:59:14 +0100

    Two sums in leetcode

---
+++ CodingStruggles.Tests/TwoSumTest.cs
@@ -0,0 +1,18 @@
+﻿using CodingStruggles;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace CodingStruggles.Tests;
+
+[TestFixture]
+[TestOf(typeof(TwoSum))]
+public class TwoSumTest
+{
+    [TestCase(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+    [TestCase(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+    [TestCase(new[] { 3, 3 }, 6, new[] { 0, 1 })]
+    public void TwoSumResolve(int[] nums, int target, int[] expected)
+    {
+        CollectionAssert.AreEquivalent(expected, TwoSum.Resolve(nums, target));
+    }
+}
---
+++ CodingStruggles/TwoSum.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace CodingStruggles;
+
+public class TwoSum
+{
+    public static int[] Resolve(int[] nums, int target)
+    {
+        Hashtable ht = new Hashtable();
+        
+        var dic = new Dictionary<int, List<int>>();
+        // nums.Select((value, index) => new { value, index })
+        // .ToDictionary(x => x.value, x => x.index);
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (dic.ContainsKey(nums[i]))
+            {
+                dic[nums[i]].Add(i);
+            }
+            else
+            {
+                dic.Add(nums[i], new List<int> { i });
+            }
+        }
+
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var desired = target - nums[i];
+            if (dic.ContainsKey(desired) && dic[desired].First() != i)
+            {
+                return [dic[desired].First(), i];
+            }
+
+            if (dic.ContainsKey(desired) && dic[desired].Count == 2)
+            {
+                return dic[desired].ToArray();
+            }
+        }
+
+        return [];
+    }
+}
